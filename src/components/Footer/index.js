d453b40c0a8aa40b8f59d0a0386ad58e13a27719
Peyton Schlafley <pschlafley0@gmail.com>
2021-02-07 13:36:10 -0500

    removed upwork svg, and created a favicon replacing the react favicon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,11 +12,6 @@ function Footer() {
                 <div className='pr-5'>
                     <a href="https://www.linkedin.com/in/peyton-schlafley-81125a1b1/" target="_blank" rel='noreferrer'><i className="color-3 fab fa-linkedin fa-3x"></i></a>
                 </div>
-                <div>
-                    <a href="https://www.upwork.com/freelancers/~0109ca639171fe8cee" target="_blank" rel='noreferrer'>
-                        Link to my UpWork
-                    </a>
-                </div>
             </div>
         </footer>
     )

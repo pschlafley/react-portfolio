1fdbccde911fb3e49f4cf76375eb19684ec37d43
Peyton Schlafley <pschlafley0@gmail.com>
2021-03-13 12:29:33 -0500

    added email to contact-Me page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,9 @@ const ContactForm = () => {
     return (
         <section className="pt-3">
             <h2 className='section-title pt-5'>Contact Me</h2>
+            <div className='text-center font-2'>
+                <p>(pschlafley0@gmail.com)</p>
+            </div>
             <div className='pt-5'>
                 <MyForm />
             </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,8 +12,8 @@ function Footer() {
                 <div className='pr-5'>
                     <a href="https://www.linkedin.com/in/peyton-schlafley-81125a1b1/" target="_blank" rel='noreferrer'><i className="color-3 fab fa-linkedin fa-3x"></i></a>
                 </div>
-                <div className='pr-5'>
-                    <a href="" target="_blank" rel='noreferrer'><i className="fab fa-mailchimp fa-3x color-3"></i></a>
+                <div id='reference' className='pr-5'>
+                    <i className="fab fa-mailchimp fa-3x color-3"></i>
                 </div>
             </div>
         </footer>

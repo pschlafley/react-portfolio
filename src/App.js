243872d8e3558f6c1a7a9462b46f9834048f86ee
Peyton Schlafley <pschlafley0@gmail.com>
2022-02-19 15:27:21 -0500

    removed unwanted packages and added BrowserRouter component to index.js file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer'
-import ContactForm from './components/Contact';
-import About from './components/About';
-import Project from './components/Project';
-import Resume from './components/Resume';
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
-
-  const [currentPage, setCurrentPage] = useState('About Me');
-
-  const tabs1 = ['About Me', 'Portfolio', 'Resume', 'Contact Me'];
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'Contact Me':
-        return <ContactForm />;
-      case 'Portfolio':
-        return <Project />;
-      case 'Resume':
-        return <Resume />;
-      default:
-        return <About />
-    }
-  };
-
   return (
-    <div className='page-container'>
-      <div className='content-wrap'>
-        <Header
-          tabs={tabs1}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        ></Header>
-        {renderPage(currentPage)}
-        <Footer />
-      </div>
+    <div>
     </div>
   );
 }
